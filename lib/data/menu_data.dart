c9571f7c187044614c2b389af1446f7d954a0f15
ninspascua <ninapascua88@gmail.com>
2025-08-29 18:42:16 +0800

    menu data

---
+++ lib/data/menu_data.dart
@@ -0,0 +1,94 @@
+final List<Map<String, dynamic>> classics = [
+  {
+    "title": "Cinnamon Roll",
+    "price": 100,
+    "image": "assets/images/menu/cinnamon_roll.jpg",
+  },
+  {
+    "title": "Blueberry Pie",
+    "price": 150,
+    "image": "assets/images/menu/blueberry_pie.jpg",
+  },
+  {
+    "title": "Homemade Strawberry Cream Cheese",
+    "price": 150,
+    "image": "assets/images/menu/homemade_strawberry_cream_cheese_pie.jpg",
+  },
+  {
+    "title": "Strawberry Cupcakes",
+    "price": 150,
+    "image": "assets/images/menu/strawberry_cupcakes_with_strawberry_buttercream_frosting.jpg",
+  },
+];
+
+final List<Map<String, dynamic>> bread = [
+  {
+    "title": "Banana Bread",
+    "price": 50,
+    "image": "assets/images/menu/banana_bread.jpg",
+  },
+  {
+    "title": "Brioche",
+    "price": 50,
+    "image": "assets/images/menu/brioche.jpg",
+  },
+  {
+    "title": "Ensaymada",
+    "price": 50,
+    "image": "assets/images/menu/ensaymada.jpg",
+  },
+  {
+    "title": "Japanese Milk Bread",
+    "price": 50,
+    "image": "assets/images/menu/japanese_milk_bread.jpg",
+  },
+];
+
+final List<Map<String, dynamic>> vegan = [
+  {
+    "title": "Pecan Pie",
+    "price": 200,
+    "image": "assets/images/menu/pecan_pie.jpeg",
+  },
+  {
+    "title": "Vegan Garlic Bread",
+    "price": 200,
+    "image": "assets/images/menu/vegan_garlic_bread.jpeg",
+  },
+  {
+    "title": "Vegan Quesitos",
+    "price": 200,
+    "image": "assets/images/menu/vegan_quesitos.jpeg",
+  },
+  {
+    "title": "Vegan Spinach Cheese Borek",
+    "price": 200,
+    "image": "assets/images/menu/vegan_spinach_cheese_borek.jpeg",
+  },
+];
+
+final List<Map<String, dynamic>> glutenFree = [
+  {
+    "title": "Chocolate Soufflé",
+    "price": 150,
+    "image": "assets/images/menu/chocolate_souffle.jpg",
+  },
+  {
+    "title": "Baked Oatmeal",
+    "price": 150,
+    "image": "assets/images/menu/baked_oatmeal.jpg",
+  },
+  {
+    "title": "Lace Cookies",
+    "price": 150,
+    "image": "assets/images/menu/lace_cookies.jpg",
+  },
+  {
+    "title": "Pumpkin Crumb Cake",
+    "price": 150,
+    "image": "assets/images/menu/pumpkin_crumb_cake.jpg",
+  },
+];
+
+
+
