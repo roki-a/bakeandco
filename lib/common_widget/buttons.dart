2676a079a5984d302cedeb421b9378e604abd844
CINEMA\franc <cinella.alejandro07@gmail.com>
2025-08-29 18:02:27 +0800

    regis_btn > buttons. made changes

---
+++ lib/common_widget/buttons.dart
@@ -0,0 +1,57 @@
+import 'package:bakeandco/common_style/color_extension.dart';
+import 'package:flutter/material.dart';
+
+enum BtnType { bgPrimary, txtPrimary }
+
+class RegisBtn extends StatelessWidget {
+  final VoidCallback onClick;
+  final String title;
+  final BtnType type;
+  final IconData? icon;
+
+  /// New optional params
+  final double? fontSize;
+  final double? width;
+  final double? height;
+
+  const RegisBtn({
+    super.key,
+    required this.title,
+    required this.onClick,
+    this.type = BtnType.bgPrimary,
+    this.icon,
+    this.fontSize,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,   // custom width
+      height: height, // custom height
+      child: ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: type == BtnType.bgPrimary ? ElementColors.primary : ElementColors.secondary,
+          foregroundColor: type == BtnType.bgPrimary ? ElementColors.tertiary : ElementColors.primary,
+          minimumSize: const Size.fromHeight(60), // default height if not overridden
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+            side: type == BtnType.bgPrimary
+                ? BorderSide.none
+                : BorderSide(color: ElementColors.primary, width: 1),
+          ),
+        ),
+        onPressed: onClick,
+        icon: icon != null ? Icon(icon, size: 20) : const SizedBox.shrink(),
+        label: Text(
+          title,
+          style: TextStyle(
+            fontSize: fontSize ?? 20, // custom font size (default 20)
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
